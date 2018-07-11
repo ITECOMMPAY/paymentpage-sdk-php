f2a9a38e7df01f7eea3bca55ae5646b5e9a4c980
Roman Zhukov <r.zhukov@it.ecommpay.com>
2018-07-11 15:34:17 +0300

    Payment types and Statuses Closes #5

--- src/Callback.php
+++ src/Callback.php
@@ -8,6 +8,71 @@ namespace ecommpay;
 class Callback
 {
     /**
+     * Successful payment
+     */
+    const SUCCESS_STATUS = 'success';
+
+    /**
+     * Rejected payment
+     */
+    const DECLINE_STATUS = 'decline';
+
+    /**
+     * Awaiting a request with the result of a 3-D Secure Verification
+     */
+    const AW_3DS_STATUS = 'awaiting 3ds result';
+
+    /**
+     * Awaiting customer return after redirecting the customer to an external provider system
+     */
+    const AW_RED_STATUS = 'awaiting redirect result';
+
+    /**
+     * Awaiting customer actions, if the customer may perform additional attempts to make a payment
+     */
+    const AW_CUS_STATUS = 'awaiting customer';
+
+    /**
+     * Awaiting additional parameters
+     */
+    const AW_CLA_STATUS = 'awaiting clarification';
+
+    /**
+     * Awaiting request for withdrawal of funds (capture) or cancellation of payment (cancel) from your project
+     */
+    const AW_CAP_STATUS = 'awaiting capture';
+
+    /**
+     * Holding of funds (produced on authorization request) is cancelled
+     */
+    const CANCELLED_STATUS = 'cancelled';
+
+    /**
+     * Successfully completed the full refund after a successful payment
+     */
+    const REFUNDED_STATUS = 'refunded';
+
+    /**
+     * Completed partial refund after a successful payment
+     */
+    const PART_REFUNDED_STATUS = 'partially refunded';
+
+    /**
+     * Payment processing at Gate
+     */
+    const PROCESSING_STATUS = 'processing';
+
+    /**
+     * An error occurred while reviewing data for payment processing
+     */
+    const ERROR_STATUS = 'error';
+
+    /**
+     * Refund after a successful payment before closing of the business day
+     */
+    const REVERSED_STATUS = 'reversed';
+
+    /**
      * Callback data as array
      *
      * @var array
