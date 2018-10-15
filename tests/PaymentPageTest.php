220843da21663d8dcabad08be4d0006c817c16b6
Roman Zhukov <zhukov.roman@gmail.com>
2018-10-15 17:35:06 +0300

    Urlencode URL params

--- tests/PaymentPageTest.php
+++ tests/PaymentPageTest.php
@@ -14,11 +14,12 @@ class PaymentPageTest extends \PHPUnit\Framework\TestCase
         $handler = new SignatureHandler('secret');
         $paymentPage = new PaymentPage($handler);
         $payment = new Payment(100);
+        $payment->setPaymentDescription('B&W');
         $url = $paymentPage->getUrl($payment);
 
         self::assertEquals(
-            'https://paymentpage.ecommpay.com/payment?project_id=100&signature=kP9TBXZQj+QqU' .
-            '/TVcaJHAmgtXqCHmM5wexUCp5eVTaTeNmVZ0axXMspVk9jd74Fu1n6ZL2Wv3HB87Wn/fXJnEA==',
+            'https://paymentpage.ecommpay.com/payment?project_id=100&payment_description=B%2526W' .
+            '&signature=X9rP65p71v5vteLWHBroNr5NE1GrqBu%2FjyFKk7BhZVgtPIFiO3iquKIAPtKkuSD7htuWiLp8DRyfL4H9vT5d3A%3D%3D',
             $url
         );
     }
