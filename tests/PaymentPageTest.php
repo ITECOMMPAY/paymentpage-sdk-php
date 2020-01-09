07d32f56e8922873925f41e1014b790e478cec79
Sergey Baronenkov <s.baronenkov@it.ecommpay.com>
2020-01-09 16:18:40 +0300

    PP-5342 done: make payment id optional

--- tests/PaymentPageTest.php
+++ tests/PaymentPageTest.php
@@ -13,14 +13,18 @@ class PaymentPageTest extends \PHPUnit\Framework\TestCase
     {
         $handler = new SignatureHandler('secret');
         $paymentPage = new PaymentPage($handler);
-        $payment = new Payment(100, 'test payment id');
-        $payment->setPaymentDescription('B&W');
+        $payment = new Payment(100);
+
+        $payment
+            ->setPaymentId('test payment id')
+            ->setPaymentDescription('B&W');
+
         $url = $paymentPage->getUrl($payment);
 
         self::assertEquals(
-            'https://paymentpage.ecommpay.com/payment?project_id=100&payment_id=test+payment+id'
-            . '&interface_type=%7B%22id%22%3A23%7D&payment_description=B%26W&signature=97JFQpAyJ4HPfGVed'
-            . 'Jh0M1MqQDOFt%2FMCbdh8VrsT7DdRyTBDAF2mvUOsDANx1ZPfbvZg0%2BVUbF43xJnq0jEeLA%3D%3D',
+            'https://paymentpage.ecommpay.com/payment?project_id=100&interface_type=%7B%22id%22%3A23%7D'
+            . '&payment_id=test+payment+id&payment_description=B%26W&signature=97JFQpAyJ4HPfGVedJh0M1MqQDOFt%2FM'
+            . 'Cbdh8VrsT7DdRyTBDAF2mvUOsDANx1ZPfbvZg0%2BVUbF43xJnq0jEeLA%3D%3D',
             $url
         );
     }
